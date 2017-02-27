92c1d24b4dbe94db0348319fdee0bb1399bcc024
鬼谷姊姊 <602760689@qq.com>
2017-02-27 09:02:41 +0800

    完成

--- task_37/index.js
+++ task_37/index.js
@@ -1,34 +1,26 @@
-var mask = document.getElementById('mask'),
-    suspend = document.getElementById('suspend'),
-    showBtn = document.getElementById('show_btn'),
-    closeBtn = document.getElementById('close_btn');
+/*获取dom结点*/
+var maskWrapper=document.getElementById("mask-wrapper"),
+    closeButton=document.getElementById("close-button"),
+    // determineButton=document.getElementById("determine-button"),
+    // cancelButton=document.getElementById("cancel-button"),
+    mask=document.getElementById("mask"),
+    login=document.getElementById("login");
 
-function showPage() {
-    mask.style.display = 'none';
-    suspend.style.display = 'none';
+/*初始遮罩层关闭*/
+maskWrapper.style.display="none";
+mask.style.display="none";
+
+/*点击登陆显示遮罩层*/
+function showMaskPage() {
+    maskWrapper.style.display = "block";
+    mask.style.display = "block";
 }
-mask.onclick = showPage;
-closeBtn.onclick = showPage;
+login.onclick=showMaskPage;
 
-showBtn.onclick = function showLogin() {
-    mask.style.display = 'block';
-    suspend.style.display = 'block';
+/*点击遮罩层或者X按钮关闭*/
+function closeMask() {
+    maskWrapper.style.display="none";
+    mask.style.display="none";
 }
-
-//拖拽
-suspend.onmousedown = drag;
-function drag(event) {
-    var disX = event.clientX - suspend.offsetLeft,
-        disY = event.clientY - suspend.offsetTop;
-    document.onmousemove = function (event) {
-        changeX = event.clientX - disX;
-        changeY = event.clientY - disY;
-        suspend.style.top = changeY + 100 +'px';
-        suspend.style.left = changeX + 150 +'px';
-    }
-    document.onmouseup = function () {
-        document.onmousemove = null;
-        document.onmouseup = null;
-    }
-
-}
+closeButton.onclick=closeMask;
+mask.onclick=closeMask;
